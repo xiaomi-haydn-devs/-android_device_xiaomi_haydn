d560e3b4668df10c06e05cc15a2688b3bf2cce67
Astridxx <muratkozan350@gmail.com>
2024-01-16 19:34:00 +0300

    haydn: Add Leica Camera patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,4 +9,13 @@ echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera.git -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
 
+# Leica patch
+echo 'Adding Leica camera patch'
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0002-Expose-aux-camera-if-packagename-is-null.patch
+patch -p1 <0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
+patch -p1 <0002-Expose-aux-camera-if-packagename-is-null.patch
+cd ../..
+
 echo 'delete vendorsetup.sh from device tree once this is done'
