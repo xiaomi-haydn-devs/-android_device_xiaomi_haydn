05b163d9f48f0488d285fdd42cbf1c77068ab82f
Astridxx <muratkozan350@gmail.com>
2024-01-16 19:35:34 +0300

    haydn: Add audio-app patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,4 +18,11 @@ patch -p1 <0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
 patch -p1 <0002-Expose-aux-camera-if-packagename-is-null.patch
 cd ../..
 
+# Audio
+echo 'Adding audio-app support'
+cd system/core
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Audio-app-main/0001-Add-audio-app-support.patch
+patch -p1 <0001-Add-audio-app-support.patch
+cd ../..
+
 echo 'delete vendorsetup.sh from device tree once this is done'
