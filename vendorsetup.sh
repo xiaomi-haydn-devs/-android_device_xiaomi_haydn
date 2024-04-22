6f2a6c32bdd675dcbc3029dbee837003064c8fd6
Alucard-Storm <supersayan1000@gmail.com>
2024-04-22 13:57:49 +0000

    haydn: Rising 2.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -29,32 +29,11 @@ echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera.git -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
 
-# Pgo-Profiles
-echo 'Cloning Pgo-Profiles from Crdroid'
-rm -rf toolchain/pgo-profiles
-git clone --depth=1 https://github.com/crdroidandroid/android_toolchain_pgo-profiles.git toolchain/pgo-profiles
-
-# Leica patch
-echo 'Adding Leica camera patch'
-cd frameworks/base
-wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
-patch -p1 <0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
-cd ../..
-
 # VoNR
 echo 'Enabling VoNR by default'
 cd frameworks/base
 wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/VoNR/0001-Enable-VoNR-by-default.patch
 patch -p1 <0001-Enable-VoNR-by-default.patch
-cd ../..
-
-# Dolby
-echo 'Adding Dolby patch'
-cd frameworks/av
-wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/Dolby/0003-media-OMXStore-Import-loading-libstagefrightdolby.patch
-wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/Dolby/0004-Import-Dolby-Effects-initialization.patch
-patch -p1 <0003-media-OMXStore-Import-loading-libstagefrightdolby.patch
-patch -p1 <0004-Import-Dolby-Effects-initialization.patch
 cd ../..
 
 # USB Charging
